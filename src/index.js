6b4c1b52e9c16e72c155097396e6b47cb51c1e48
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-11-30 19:46:12 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 require('dotenv').config({ path: './env' });
+const app = require('./app');
 const connectDB = require("./db");
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("MONGODB Connection FAILED: " + error)
+    });
